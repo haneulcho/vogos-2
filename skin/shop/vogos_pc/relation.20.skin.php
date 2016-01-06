3f6d131c9009e0ea6d1885bd8ea4c716ddfdcc38
Haneul Cho <stuykr@naver.com>
2016-01-06 11:22:57 +0900

    pc 카테고리별 상품 출력 버그 수정, 페이지로 넘기기

--- skin/shop/vogos_pc/relation.20.skin.php
+++ skin/shop/vogos_pc/relation.20.skin.php
@@ -3,7 +3,8 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 ?>
 <script src="<?php echo G5_SHOP_SKIN_URL; ?>/js/owl.carousel.min.js"></script>
 <?php
-    $owlcateWidth = ($this->list_mod * $this->img_width) + ($this->list_mod - 1) * 20;
+    //$owlcateWidth = ($this->list_mod * $this->img_width);
+$owlcateWidth = 890;
     echo '<div class="owlcate_btns fullWidth"><div class="owlcate_btn nextBtn"><i class="ion-ios-arrow-right"></i></div>'.PHP_EOL;
     echo '<div class="owlcate_btn prevBtn"><i class="ion-ios-arrow-left"></i></div></div>'.PHP_EOL;
 for ($i=1; $row=sql_fetch_array($result); $i++) {
@@ -92,17 +93,33 @@ $(function(){
     nav: true,
     autoplay: true,
     smartSpeed: 300,
-    margin: 0
+    dotsSpeed: 240,
+    margin: 10
     });
 
+    var $dots = $('.owl-dot');
+    function goToPage(type) {
+        if(type == 'next') {
+            var $next = $dots.filter('.active').next();
+            if (!$next.length)
+                $next = $dots.first();
+            $next.trigger('click');
+        } else {
+            var $prev = $dots.filter('.active').prev();
+            if (!$prev.length)
+                $prev = $dots.last();
+            $prev.trigger('click');
+        }
+    }
+
     $('.nextBtn').click(function() {
-        sync2.trigger('next.owl.carousel');
+        //sync2.trigger('next.owl.carousel');
+        goToPage('next');
     })
     // Go to the previous item
     $('.prevBtn').click(function() {
-        // With optional speed parameter
-        // Parameters has to be in square bracket '[]'
-        sync2.trigger('prev.owl.carousel', [500]);
+        //sync2.trigger('prev.owl.carousel', [500]);
+        goToPage('prev');
     })
 });
 </script>
